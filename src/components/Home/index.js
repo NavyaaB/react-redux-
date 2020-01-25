35799b7ee9505f6c7047968c408e1b8ac4e6895f
NavyaaB <navyabachati@gmail.com>
2020-01-24 20:22:10 -0500

    added styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,18 +2,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchData } from '../../constants/postActions';
+import styles from '../styles.css';
 
 class Home extends React.Component {
-    handleClick = (e,id) => {
+    handleClick = (e, id) => {
         e.preventDefault();
         this.props.deleteVehicle(id);
     }
-    
+
     componentDidMount() {
         // console.log('mounted');
         this.props.fetchData();
     }
-    
+
     componentWillUnmount() {
         this.props.onUnload();
     }
@@ -26,23 +27,25 @@ class Home extends React.Component {
                     <td> {vehicle.model} </td>
                     <td> {vehicle.make} </td>
                     <td> {vehicle.year} </td>
-                    <td> <button onClick={(e)=>this.handleClick(e, vehicle.id)}>Delete</button> </td>
+                    <td> <button className="delete-btn" onClick={(e) => this.handleClick(e, vehicle.id)}>Delete</button> </td>
                 </tr>
             </tbody>
-            ))
+        ))
         return (
-            <div>
-            <table>
-                <thead>
-                    <tr className="table-heading">
-                        <th>Make</th>
-                        <th>Model</th>
-                        <th>Year</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                    {vehiclesItems}
-            </table>
+            <div className="container">
+                <div className="home">
+                    <table>
+                        <thead>
+                            <tr className="table-heading">
+                                <th>Make</th>
+                                <th>Model</th>
+                                <th>Year</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        {vehiclesItems}
+                    </table>
+                </div>
             </div>
         );
     }
@@ -53,7 +56,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    deleteVehicle: id => dispatch({ type: 'REMOVE_VEHICLE', id: id}),
+    deleteVehicle: id => dispatch({ type: 'REMOVE_VEHICLE', id: id }),
     fetchData: () => dispatch(fetchData())
 });
 
